82e10f14c2aadfac1952c1bb0c5d041452a0f267
suhaillahmad <suhaillahmadd0@gmail.com>
2023-04-19 10:32:24 +0530

    Removed Required for domain and roll no in registration

--- Core/models.py
+++ Core/models.py
@@ -188,7 +188,7 @@ class Registration(models.Model):
     college_email = models.EmailField(unique=True)
     # roll_no = models.CharField(max_length=13,default="123")
     #changing just for first year registration
-    roll_no = models.CharField(max_length=13, null=True)
+    roll_no = models.CharField(max_length=13, null=True, blank=True)
     student_number = models.CharField(max_length=10, unique=True)
     phone = models.CharField(max_length=10, null=False)
     branch = models.ForeignKey('Branch',on_delete=models.CASCADE)
@@ -198,7 +198,7 @@ class Registration(models.Model):
     # Default Year is 1 for SI Workshop
     year = models.ForeignKey('Year',default=1,on_delete=models.CASCADE)
     # Registration for workshop doesn't require Domain so Null=True
-    domain = models.ForeignKey('Domain',related_name='registrations',on_delete=models.CASCADE, null=True)
+    domain = models.ForeignKey('Domain',related_name='registrations',on_delete=models.CASCADE, null=True, blank=True)
     skills = models.CharField(max_length=250,default="HTML",help_text='Skills like HTML, CSS, Java...')
     hacker_rank_username = models.CharField(max_length=250,null=True,blank=True,help_text='Your HackerRank username. Please create an account on HackerRank.')
     github_username = models.CharField(max_length=250,null=True,blank=True,help_text='Your Github username. Please create an account on Github.')
