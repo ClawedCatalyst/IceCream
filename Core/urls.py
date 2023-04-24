cc6c732099e56e4d20a59df29dd299b4305db0dd
anshumannandan <anshuman21xxx034@akgec.ac.in>
2023-04-24 11:54:47 +0530

    updated logo url

--- Core/urls.py
+++ Core/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='home'),
     url(r'^savecontact$', SaveContactView.as_view(), name='save_contact'),
-    url(r'^[register|submit]', RegistrationView.as_view(), name='registration'),
+    url(r'^register', RegistrationView.as_view(), name='registration'),
+    url(r'^submit', RegistrationView.as_view(), name='registration'),
     url(r'^blogs', BlogView.as_view(), name='blog'),
     url(r'^blogs/(?P<pk>\d+)/detail', BlogDetailView.as_view(), name='blog_detail'),
     url(r'^feedback', FeedbackView.as_view(), name='feedback'),
