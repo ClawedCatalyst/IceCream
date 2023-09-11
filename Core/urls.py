0c38ecdeadda5df7320347b0f67878dd84db3676
personal <suhaillahmadd0@gmail.com>
2023-09-11 15:48:44 +0530

    Commented url for registration view

--- Core/urls.py
+++ Core/urls.py
@@ -1,10 +1,10 @@
-from .views import IndexView,  BlogView, BlogDetailView, SaveContactView, FeedbackView, AlumniView, RegistrationView, EmailTemplatesView
+from .views import IndexView,  BlogView, BlogDetailView, SaveContactView, FeedbackView, AlumniView, EmailTemplatesView
 from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='home'),
     url(r'^savecontact$', SaveContactView.as_view(), name='save_contact'),
-    url(r'^register', RegistrationView.as_view(), name='registration'),
+    # url(r'^register', RegistrationView.as_view(), name='registration'),
     # url(r'^submit', RegistrationView.as_view(), name='registration'),
     url(r'^blogs', BlogView.as_view(), name='blog'),
     url(r'^blogs/(?P<pk>\d+)/detail', BlogDetailView.as_view(), name='blog_detail'),
